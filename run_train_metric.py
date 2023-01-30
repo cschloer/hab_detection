1d7300b34f45792106bfaa2a5492d20b913c95db
Conrad S <conrad.schloer@gmail.com>
2023-01-30 13:53:32 +0100

    Test test

--- run_train_metric.py
+++ run_train_metric.py
@@ -7,6 +7,7 @@ from metrics import get_model_performance
 from constants import (
     device,
     ZIP_PATH_TRAIN,
+    ZIP_PATH_TEST,
     TRAINING_BATCH_SIZE,
     MODEL_SAVE_FOLDER,
     MODEL_LOAD_PATH,
@@ -14,7 +15,7 @@ from constants import (
 
 log(f"Loading datasets...")
 
-train_dataset = get_image_dataset(ZIP_PATH_TRAIN)
+train_dataset = get_image_dataset(ZIP_PATH_TEST)
 train_loader = DataLoader(
     train_dataset,
     batch_size=TRAINING_BATCH_SIZE,
