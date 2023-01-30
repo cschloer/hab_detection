35ab5c592af6126d097247842959e05584c74775
Conrad S <conrad.schloer@gmail.com>
2023-01-30 14:06:56 +0100

    Remove break

--- metrics.py
+++ metrics.py
@@ -75,7 +75,7 @@ def get_model_performance(model, loader):
             sum_average_distance += average_distance.item()
             sum_average_distance_without_hab += average_distance_without_hab.item()
 
-            break
+            #break
             del inputs
             del labels
             del preds
