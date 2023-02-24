b427226f181a7df266a038b7c05fef831347724d
Conrad S <conrad.schloer@gmail.com>
2023-02-24 10:25:41 +0100

    update run_visualize scrpt

--- run_experiment.py
+++ run_experiment.py
@@ -3,43 +3,38 @@ import json
 from hab_detection.train import train
 
 
-def get_input_experiment(args, script_name):
-    with open("experiments.json", "r") as f:
-        experiments = json.load(f)
+with open("experiments.json", "r") as f:
+    experiments = json.load(f)
 
-    # First argument is experiment id, second argument is the epoch start
+# First argument is experiment id, second argument is the epoch start
 
-    if len(args) < 2:
-        print(f"{script_name} requires atleast 1 argument, the experiment id.")
+if len(sys.argv) < 2:
+    print(f"run_experiment.py requires atleast 1 argument, the experiment id.")
+    exit()
+if len(sys.argv) > 4:
+    print(
+        "run_experiment.py takes maximally 3 arguments, the experiment id, the start epoch, and the filename/path of the model to preload ."
+    )
+    exit()
+
+e = sys.argv[1]
+epoch_start = 0
+if len(sys.argv) >= 3:
+    epoch_start_str = sys.argv[2]
+    if not epoch_start_str.isdigit():
+        print("The second argument must be an integer")
         exit()
-    if len(args) > 4:
-        print(
-            "{script_name} takes maximally 3 arguments, the experiment id, the start epoch, and the filename/path of the model to preload ."
-        )
-        exit()
+    epoch_start = int(epoch_start_str)
 
-    e = args[1]
-    epoch_start = 0
-    if len(args) >= 3:
-        epoch_start_str = args[2]
-        if not epoch_start_str.isdigit():
-            print("The second argument must be an integer")
-            exit()
-        epoch_start = int(epoch_start_str)
+model_file = None
+if len(sys.argv) == 4:
+    model_file = sys.argv[3]
 
-    model_file = None
-    if len(args) == 4:
-        model_file = args[3]
+experiment_name = sys.argv[1]
+if experiment_name not in experiments:
+    print(f"Experiment {experiment_name} not in experiments.json")
 
-    experiment_name = args[1]
-    if experiment_name not in experiments:
-        print(f"Experiment {experiment_name} not in experiments.json")
-
-    e = experiments[experiment_name]
-    return e, epoch_start, model_file
-
-
-e, epoch_start, model_file = get_input_experiment(sys.argv, "run_experiment.py")
+e = experiments[experiment_name]
 
 
 train(
--- run_visualize.py
+++ run_visualize.py
@@ -3,8 +3,38 @@ import json
 from hab_detection.visualize import visualize
 from .run_experiment import get_input_experiment
 
-e, epoch, model_file = get_input_experiment(sys.argv, "run_visualize")
+with open("experiments.json", "r") as f:
+    experiments = json.load(f)
 
+# First argument is experiment id, second argument is the epoch start
+
+if len(sys.argv) < 2:
+    print(f"run_visualize.py requires atleast 1 argument, the experiment id.")
+    exit()
+if len(sys.argv) > 4:
+    print(
+        "run_visualize.py takes maximally 3 arguments, the experiment id, the start epoch, and the filename/path of the model to preload ."
+    )
+    exit()
+
+e = sys.argv[1]
+epoch_start = 0
+if len(sys.argv) >= 3:
+    epoch_start_str = sys.argv[2]
+    if not epoch_start_str.isdigit():
+        print("The second argument must be an integer")
+        exit()
+    epoch_start = int(epoch_start_str)
+
+model_file = None
+if len(sys.argv) == 4:
+    model_file = sys.argv[3]
+
+experiment_name = sys.argv[1]
+if experiment_name not in experiments:
+    print(f"Experiment {experiment_name} not in experiments.json")
+
+e = experiments[experiment_name]
 
 visualize(
     e["name"],
