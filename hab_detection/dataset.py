5659cfcc03c0200a0c4fa270a6bae2f084f21f57
Conrad S <conrad.schloer@gmail.com>
2023-02-27 16:52:55 +0100

    Add print

--- hab_detection/dataset.py
+++ hab_detection/dataset.py
@@ -115,11 +115,13 @@ class ImageData(Dataset):
         # label = torch.from_numpy(label)
         # F.one_hot(label, num_classes=len(class_designation))
 
+        print("______________________")
         print(label.shape)
         label = label.reshape(label.shape[1], label.shape[2]).astype(np.int_)
         print(label.shape)
         label = TF.to_tensor(label)
         print(label.shape)
+        print("______________________")
         return label
 
     def random_transform(self, image, label):
