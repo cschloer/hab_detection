3f21d6d197c202072861e896ad6b1627e30415a6
Conrad S <conrad.schloer@gmail.com>
2023-03-15 12:12:49 +0100

    Push

--- hab_detection/dataset.py
+++ hab_detection/dataset.py
@@ -35,7 +35,7 @@ def get_data(zip_path):
                     f'Corresponding label file doesn\'t exist: "{label_filename}"'
                 )
 
-            if f.startswith("winnebago_2019_7_25_x32_y1600_64x64_28"):
+            if f.startswith("winnebago_2019_7_25_x32"):
                 imgs.append(f)
                 labels.append(label_filename)
 
