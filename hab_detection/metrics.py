7abfb82d00ef5623e2876f4325db5ec97c9ab23f
Conrad S <conrad.schloer@gmail.com>
2023-04-04 20:52:32 +0200

    Push automatically - developing on a server

--- hab_detection/metrics.py
+++ hab_detection/metrics.py
@@ -103,10 +103,8 @@ def get_model_performance(
                 print(torch.mean(inputs))
                 print(torch.var(inputs))
                 print("pre normalization")
-                print(type(raw_input_))
-                print(raw_input_)
-                print(np.mean(raw_input_))
-                print(np.var(raw_input_))
+                print(torch.mean(raw_input_))
+                print(torch.var(raw_input_))
                 print("___________________________________")
 
             preds = model.predict(inputs)  # make prediction
