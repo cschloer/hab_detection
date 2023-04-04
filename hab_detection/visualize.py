46e27f8584c740bbebbec45b506fe2f2648cb024
Conrad S <conrad.schloer@gmail.com>
2023-04-04 20:58:37 +0200

    Push automatically - developing on a server

--- hab_detection/visualize.py
+++ hab_detection/visualize.py
@@ -159,7 +159,7 @@ def visualize_image(
             dataset.transform_label(torch.from_numpy(cyan_reshaped).int()), 0
         ).to(device)
 
-        print(torch.squeeze(label.shape), transformed_sen2.shape)
+        print(torch.squeeze(label).shape, transformed_sen2.shape)
         print("MEAN VARIANCE IN FULL IMAGE")
         print(torch.mean(transformed_sen2_batch))
         print(torch.var(transformed_sen2_batch))
