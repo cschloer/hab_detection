3f1c6551f1f2d964844b84ac3d55d22fd8699bda
Conrad S <conrad.schloer@gmail.com>
2023-04-11 22:11:00 +0200

    Push automatically - developing on a server

--- dataset_create/create_dataset.py
+++ dataset_create/create_dataset.py
@@ -65,57 +65,60 @@ def manage_triggers(api, name):
     while True:
         # Check if previously triggered items are now online
         delete_waiting = []
-        for k in waiting.keys():
-            try:
-                if handle_online(waiting[k]):
-                    delete_waiting.append(k)
-            except:
-                pass
-        for k in delete_waiting:
-            del waiting[k]
-
-        ##########################################
-
-        # Find new things to trigger
-        while True:
-            with lock_trigger_list:
-                if len(trigger_list):
-                    r = trigger_list.pop()
-                    with lock_existing_prefixes:
-                        if r["file_prefix"] in existing_prefixes:
-                            with lock_total_downloaded:
-                                total_downloaded += 1
-                                log(
-                                    f"File with prefix {r['file_prefix']} already exists in zip - {total_downloaded}"
-                                )
-                            continue
-                else:
-                    break
-
-            if not handle_online(r):
+        try:
+            for k in waiting.keys():
                 try:
-                    # Trigger it!
-                    log(f"Triggering LTA for {r['uuid']}")
-                    api.download(r["uuid"])
-                except LTATriggered as e:
-                    # Succesfully triggered
-                    if r["file_prefix"] in waiting:
-                        log(
-                            f"FOUND FILE PREFIX TWICE: {r} ------------- {waiting[r['file_prefix']]}"
-                        )
-                    else:
-                        waiting[r["file_prefix"]] = r
+                    if handle_online(waiting[k]):
+                        delete_waiting.append(k)
+                except:
+                    pass
+            for k in delete_waiting:
+                del waiting[k]
 
-                except LTAError as e:
-                    # No more trigger credits, add back to trigger list
-                    with lock_trigger_list:
-                        trigger_list.append(r)
-                    # Break out of loop to sleep
-                    break
-                except Exception as e:
-                    log(f"THERE WAS AN ERROR: {e}")
-                    with lock_trigger_list:
-                        trigger_list.append(r)
+            ##########################################
+
+            # Find new things to trigger
+            while True:
+                with lock_trigger_list:
+                    if len(trigger_list):
+                        r = trigger_list.pop()
+                        with lock_existing_prefixes:
+                            if r["file_prefix"] in existing_prefixes:
+                                with lock_total_downloaded:
+                                    total_downloaded += 1
+                                    log(
+                                        f"File with prefix {r['file_prefix']} already exists in zip - {total_downloaded}"
+                                    )
+                                continue
+                    else:
+                        break
+
+                if not handle_online(r):
+                    try:
+                        # Trigger it!
+                        log(f"Triggering LTA for {r['uuid']}")
+                        api.download(r["uuid"])
+                    except LTATriggered as e:
+                        # Succesfully triggered
+                        if r["file_prefix"] in waiting:
+                            log(
+                                f"FOUND FILE PREFIX TWICE: {r} ------------- {waiting[r['file_prefix']]}"
+                            )
+                        else:
+                            waiting[r["file_prefix"]] = r
+
+                    except LTAError as e:
+                        # No more trigger credits, add back to trigger list
+                        with lock_trigger_list:
+                            trigger_list.append(r)
+                        # Break out of loop to sleep
+                        break
+                    except Exception as e:
+                        log(f"THERE WAS AN ERROR: {e}")
+                        with lock_trigger_list:
+                            trigger_list.append(r)
+        except Exception as e:
+            log(f"THERE WAS AN ERROR: {e}")
         time.sleep(60)
         log(f"Waking up in LTA thread...")
 
@@ -128,67 +131,70 @@ def manage_downloads(api, name, lock_zip):
     log = create_logger(f"-- Download Thread {name}")
     while True:
         while True:
-            r = None
-            with lock_dl_ready:
-                if len(dl_ready):
-                    r = dl_ready.pop()
-            if r is not None:
-                is_online = api.is_online(r["uuid"])
-                # Put it back in the queue
-                if not is_online:
-                    log(
-                        f"Putting a file back into the LTA trigger queue: {r['file_prefix']}"
-                    )
-                    with lock_trigger_list:
-                        trigger_list.append(r)
-                else:
-                    with lock_existing_prefixes:
-                        if r["file_prefix"] in existing_prefixes:
+            try:
+                r = None
+                with lock_dl_ready:
+                    if len(dl_ready):
+                        r = dl_ready.pop()
+                if r is not None:
+                    is_online = api.is_online(r["uuid"])
+                    # Put it back in the queue
+                    if not is_online:
+                        log(
+                            f"Putting a file back into the LTA trigger queue: {r['file_prefix']}"
+                        )
+                        with lock_trigger_list:
+                            trigger_list.append(r)
+                    else:
+                        with lock_existing_prefixes:
+                            if r["file_prefix"] in existing_prefixes:
+                                with lock_total_downloaded:
+                                    total_downloaded += 1
+                                    log(
+                                        f"File with file_prefix {r['file_prefix']} already exists in zip - {total_downloaded}"
+                                    )
+                                continue
+                            else:
+                                existing_prefixes.add(r["file_prefix"])
+
+                        try:
+                            with lock_total_downloaded:
+                                log(
+                                    f"Downloading file {r['file_prefix']} - {total_downloaded + 1}"
+                                )
+                                log(r)
+                            download_and_process(
+                                api,
+                                r["uuid"],
+                                r["id"],
+                                r["file_prefix"],
+                                r["window"],
+                                r["cyan_id"],
+                                r["date"],
+                                ZIP_FILE_TRAIN
+                                if r["designation"] == "train"
+                                else ZIP_FILE_TEST,
+                                f"{SAVE_FOLDER}/images/scenes/{r['id']}/{r['date'].year}-{r['date'].month}-{r['date'].day}",
+                                create_logger(
+                                    f"-- Download Thread {name} - {r['file_prefix']}"
+                                ),
+                                lock_zip,
+                            )
                             with lock_total_downloaded:
                                 total_downloaded += 1
-                                log(
-                                    f"File with file_prefix {r['file_prefix']} already exists in zip - {total_downloaded}"
-                                )
-                            continue
-                        else:
-                            existing_prefixes.add(r["file_prefix"])
 
-                    try:
-                        with lock_total_downloaded:
-                            log(
-                                f"Downloading file {r['file_prefix']} - {total_downloaded + 1}"
-                            )
-                            log(r)
-                        download_and_process(
-                            api,
-                            r["uuid"],
-                            r["id"],
-                            r["file_prefix"],
-                            r["window"],
-                            r["cyan_id"],
-                            r["date"],
-                            ZIP_FILE_TRAIN
-                            if r["designation"] == "train"
-                            else ZIP_FILE_TEST,
-                            f"{SAVE_FOLDER}/images/scenes/{r['id']}/{r['date'].year}-{r['date'].month}-{r['date'].day}",
-                            create_logger(
-                                f"-- Download Thread {name} - {r['file_prefix']}"
-                            ),
-                            lock_zip,
-                        )
-                        with lock_total_downloaded:
-                            total_downloaded += 1
+                        except Exception as e:
+                            log(f"GOT AN ERROR: {e}")
+                            with lock_trigger_list:
+                                with lock_existing_prefixes:
+                                    existing_prefixes.remove(r["file_prefix"])
 
-                    except Exception as e:
-                        log(f"GOT AN ERROR: {e}")
-                        with lock_trigger_list:
-                            with lock_existing_prefixes:
-                                existing_prefixes.remove(r["file_prefix"])
-
-                            trigger_list.append(r)
-            else:
-                # Sleep a little
-                break
+                                trigger_list.append(r)
+                else:
+                    # Sleep a little
+                    break
+            except Exception as e:
+                log(f"GOT AN ERROR: {e}")
         time.sleep(30)
         log(f"Waking up in download thread...")
 
@@ -197,7 +203,8 @@ with open(f"{SAVE_FOLDER}/data.json", "r") as f:
     scenes = json.load(f)
 
 api = get_api(
-    os.environ.get("ESA_USER1").strip('"'), os.environ.get("ESA_PASSWORD1").strip('"'),
+    os.environ.get("ESA_USER1").strip('"'),
+    os.environ.get("ESA_PASSWORD1").strip('"'),
 )
 
 print("Creating list of products to download")
@@ -239,14 +246,20 @@ print(f"# Existing Prefixes: {len(existing_prefixes)}")
 
 # LTA Thread 1
 try:
-    thread_triggers1 = Thread(target=manage_triggers, args=(api, "1"),)
+    thread_triggers1 = Thread(
+        target=manage_triggers,
+        args=(api, "1"),
+    )
     thread_triggers1.start()
 except:
     print("Failed to make thread 1")
 
 time.sleep(10)
 # Download threads
-thread_downloads1 = Thread(target=manage_downloads, args=(api, "1", lock_zip),)
+thread_downloads1 = Thread(
+    target=manage_downloads,
+    args=(api, "1", lock_zip),
+)
 thread_downloads1.start()
 
 """
