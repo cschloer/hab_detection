032831caefe8bc736c89df7cb58af31a4f1b93bf
Conrad S <conrad.schloer@gmail.com>
2023-04-20 12:20:03 +0200

    Push automatically - developing on a server

--- hab_detection/visualize.py
+++ hab_detection/visualize.py
@@ -89,18 +89,18 @@ def visualize_full_image(
     cyan_np = np.load(label_path)
     pred_np = np.empty(cyan_np.shape)
 
-        batch = np.empty((1, *sen2_np.shape), dtype=sen2_np.dtype)
-        print(batch.shape)
-        for x in range(0, sen2_np.shape[1], 64):
-            for y in range(0, sen2_np.shape[2], 64):
-                batch.append(sen2_np[:,x:x+64,y:y+64], axis=0)
-                print(batch.shape)
-                if batch.shape[0] == 32:
-                    return
-                with torch.no_grad():
-                    model.eval()
-                    pass
-        print(sen2_np.shape)
+    batch = np.empty((1, *sen2_np.shape), dtype=sen2_np.dtype)
+    print(batch.shape)
+    for x in range(0, sen2_np.shape[1], 64):
+        for y in range(0, sen2_np.shape[2], 64):
+            batch.append(sen2_np[:,x:x+64,y:y+64], axis=0)
+            print(batch.shape)
+            if batch.shape[0] == 32:
+                return
+            with torch.no_grad():
+                model.eval()
+                pass
+    print(sen2_np.shape)
     return
 
     tracker = get_metric_tracker(class_designation)
