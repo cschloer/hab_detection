f393fb2cdde964e64efc28f736ef39687dbd164f
Conrad S <conrad.schloer@gmail.com>
2023-07-31 17:58:52 +0200

    Push automatically - developing on a server

--- hab_detection/visualize.py
+++ hab_detection/visualize.py
@@ -375,6 +375,7 @@ def visualize_image(
     pred_np,
 ):
     fig, axs = plt.subplots(2, 2, figsize=(20, 16))
+    print(fig)
     height = sen2_np.shape[1]
     width = sen2_np.shape[2]
     ycrop = height % 8
@@ -398,11 +399,8 @@ def visualize_image(
     for i in range(len(class_designation)):
         c_val = class_designation[i - 1] if i != 0 else 0
         cyan_classed = np.where(cyan_reshaped >= c_val, i, cyan_classed)
-    print(cyan_classed.shape)
-    print(pred_np.shape)
     cyan_classed = np.absolute(cyan_classed - pred_np)
     cyan_classed = np.where(cyan_reshaped == 255, len(class_designation), cyan_classed)
-    print(cyan_classed)
     classed_colormap = np.array(
         [
             [255, 250, 240, 255],
