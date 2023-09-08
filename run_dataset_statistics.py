731f42998196a61d5c6672d3941494777355e8df
Conrad S <conrad.schloer@gmail.com>
2023-09-08 10:18:13 +0200

    Push automatically - developing on a server

--- run_dataset_statistics.py
+++ run_dataset_statistics.py
@@ -63,9 +63,8 @@ for batch_idx, (_, labels, _, raw_labels) in enumerate(loader):
     )
 
     # all_mask = np.logical_or(raw_labels == 254, raw_labels == 255)
-    print(raw_labels.numpy())
-    premax = max(premax, np.max(raw_labels.numpy()))
-    postmax = max(postmax, np.max(raw_labels[~mask].numpy()))
+    premax = max(premax, torch.max(raw_labels))
+    postmax = max(postmax, torch.max(raw_labels[~mask]))
     all_dist_temp = np.bincount(
         raw_labels[~mask].flatten(),
         minlength=254,
