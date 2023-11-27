7bad684db8d472cde8bdc334f12f4d9f122927b9
Conrad S <conrad.schloer@gmail.com>
2023-11-27 13:06:36 +0100

    Push automatically - developing on a server

--- visualize/graph.py
+++ visualize/graph.py
@@ -5,12 +5,18 @@ from dist import dist
 
 fig, ax = plt.subplots()
 color = ["green"] * 100 + ["orange"] * 100 + ["red"] * 54
-low = [149, 149, 149, 255]
+low = np.array([149, 149, 149, 255]) / 255
+moderate = np.array([236, 173, 10, 255]) / 255
+high = np.array([169, 0, 0, 255]) / 255
 # moderate =
-ax.plot(range(100), dist[:100], color=low)
-ax.plot(range(100, 200), dist[100:200], color="orange")
-ax.plot(range(200, 254), dist[200:254], color="red")
+ax.plot(range(100), dist[:100], color=low, label="0-99 HAB, low risk")
+ax.plot(
+    range(100, 200), dist[100:200], color=moderate, label="100-199 HAB, moderate risk"
+)
+ax.plot(range(200, 254), dist[200:254], color=high, label="200-253 HAB, high risk")
 ax.set_yscale("log")
-ax.set_ylabel("Distribution")
+ax.set_ylabel("Pixel Distribution in Dataset")
 ax.set_xlabel("HAB Value")
+ax.set_title("Pixel Distribution by HAB Level in Final Dataset")
+ax.legend()
 plt.show()
