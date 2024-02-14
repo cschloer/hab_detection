09e34c8cc953847f3a60e85c3233dbf987b4c565
Conrad Schloer <conrad.schloer@gmail.com>
2024-02-14 10:48:53 +0100

    Push automatically - developing on a server

--- run_regional.py
+++ run_regional.py
@@ -1,11 +1,13 @@
 import sys
 import re
 import json
+from hab_detection.metrics import get_model_performance
 from hab_detection.visualize import visualize
-from hab_detection.dataset import get_data
+from hab_detection.dataset import get_data, ImageData
 from hab_detection.constants import (
     STRUCTURED_FOLDER_PATH_TEST,
 )
+from torch.utils.data import DataLoader
 
 with open("experiments.json", "r") as f:
     experiments = json.load(f)
@@ -25,6 +27,9 @@ if experiment_name not in experiments:
 
 e = experiments[experiment_name]
 class_designation = e["class_designation"]
+
+model = load_model(e["model_architecture"], model_file, None, class_designation)
+
 features, labels, _ = get_data(
     STRUCTURED_FOLDER_PATH_TEST,
 )
@@ -43,5 +48,35 @@ for i in range(len(features)):
     results[region]["features"].append(feature)
     results[region]["labels"].append(labels[i])
 
-for k in results.keys():
+region_statistics = {}
+for region in results.keys():
     print(k, len(results[k]["features"]))
+    region_statistics[region] = {}
+    features = results[region]
+    print(f"Running statistics for region {region}")
+    dataset = ImageData(
+        results[region]["features"],
+        results[region]["labels"],
+        class_designation,
+        zip_path=None,
+        randomize=False,
+        transform=True,
+        in_memory=False,
+    )
+    loader = DataLoader(
+        dataset,
+        batch_size=128,
+        shuffle=False,
+        num_workers=0,
+        drop_last=False,
+    )
+    _, metrics, hist_2d = get_model_performance(
+        model,
+        loader,
+        class_designation,
+        num_batches=-1,
+        calculate_2d_hist=True,
+        calculate_statistics=True,
+    )
+    print(metrics)
+    print("----------------")
