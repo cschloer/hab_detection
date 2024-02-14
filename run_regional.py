ec2c5df57d99e3010ccf2550695a465ccbe148c6
Conrad Schloer <conrad.schloer@gmail.com>
2024-02-14 10:27:05 +0100

    Push automatically - developing on a server

--- run_regional.py
+++ run_regional.py
@@ -2,28 +2,21 @@ import sys
 import json
 from hab_detection.visualize import visualize
 from hab_detection.dataset import get_data
+from hab_detection.constants import (
+    STRUCTURED_FOLDER_PATH_TEST,
+)
 
 with open("experiments.json", "r") as f:
     experiments = json.load(f)
 
 
-if len(sys.argv) < 2:
-    print(f"run_regional.py requires atleast 1 argument, the experiment id.")
-    exit()
-if len(sys.argv) > 4:
+if len(sys.argv) != 3:
     print(
-        "run_regional.py takes maximally 3 arguments, the experiment id, the dataset type, and the filename/path of the model to preload ."
+        f"run_regional.py requires exactly 2 arguments, the experiment id and the model path."
     )
     exit()
 
-e = sys.argv[1]
-dataset_type = "test"
-if len(sys.argv) >= 3:
-    dataset_type = sys.argv[2]
-
-model_file = "epoch_recent.pt"
-if len(sys.argv) == 4:
-    model_file = sys.argv[3]
+model_file = sys.argv[2]
 
 experiment_name = sys.argv[1]
 if experiment_name not in experiments:
