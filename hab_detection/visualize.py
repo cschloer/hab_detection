ce25693f132b9c52bac07a4b6f93ee74bf3aec01
Conrad Schloer <conrad.schloer@gmail.com>
2024-02-19 15:03:37 +0100

    Push automatically - developing on a server

--- hab_detection/visualize.py
+++ hab_detection/visualize.py
@@ -790,9 +790,6 @@ def visualize(
             )
             # Plot thicker line inside of range where it is correct
             if i != 0:
-                print(
-                    f" i: {i}, floor: {floor}, ceil: {ceil}, len normalized: {len(normalized)}"
-                )
                 axs.plot(
                     range(floor, min(ceil + 1, len(normalized))),
                     normalized[floor : min(ceil + 1, len(normalized))],
